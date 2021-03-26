76433731e86783fa9a1cad615b9582cf662a8393
Calon, Rob <Rob.Calon@tennet.eu>
2021-03-26 16:12:23 +0000

    setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyetm',
+    version='0.1.0',    
+    description='Python connector for ETM API',
+    url='https://github.com/robcalon/pyetm',
+    author='Rob Calon',
+    author_email='robcalon@protonmail.com',
+    license='MIT',
+    python_requires='>=3.7',
+    install_requires=['numpy>=0.11',
+                      'pandas>=0.17',
+                      'aiohttp',
+                      'nest_asyncio'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
