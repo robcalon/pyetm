4cb8b12493e211e142ea3a99688014eb892dcebb
Rob Calon <robcalon@Robs-MacBook-Pro.local>
2022-02-03 18:40:23 +0100

    regionalizer
    bugfixes and version bump

--- pyETM/utils/__init__.py
+++ pyETM/utils/__init__.py
@@ -1,8 +1,9 @@
 from .categorization import Categorization, categorize_curves
 from .interpolation import Interpolator, interpolate_clients
+from .regionalization import Regionalization, regionalize_curves
 from .templates import Templates
 
-class Utils(Categorization, Templates):
+class Utils(Categorization, Regionalization, Templates):
     
     def __init__(self):
         super().__init__()
