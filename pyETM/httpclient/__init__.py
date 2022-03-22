638f119bbc787d5d608a7036e80278ffe009ea51
Rob Calon <31725587+robcalon@users.noreply.github.com>
2022-03-22 11:31:09 +0100

    Update __init__.py

--- pyETM/httpclient/__init__.py
+++ pyETM/httpclient/__init__.py
@@ -10,15 +10,3 @@ class HTTPClient(Context, Delete, Get, Post, Put, Response):
     
     def __init__(self):
         super().__init__()
-        
-    def __enter__(self):
-        """Contextual enter"""
-        return self
-    
-    def __exit__(self, *args, **kwargs):
-        """Contextual exit"""
-        return None
-        
-    def _make_url(self, post):
-        """Merge base url and post"""   
-        return self._base_url + post
