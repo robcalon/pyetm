940df1d30a6286cf6181adc9962782dde37184d9
Calon, Rob <Rob.Calon@tennet.eu>
2022-09-30 09:45:32 +0000

    lookup func
    function to lookup coordinates in dataframe

--- pyETM/utils/__init__.py
+++ pyETM/utils/__init__.py
@@ -1,4 +1,5 @@
 from .categorisation import categorise_curves, diagnose_categorisation
 from .interpolation import Interpolator, interpolate_clients
+from .lookup import lookup_coordinates
 from .regionalisation import (regionalise_curves, regionalise_node,
                               diagnose_regionalisation)
---
+++ pyETM/utils/lookup.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+def lookup_coordinates(coords: pd.Series, 
+        frame: pd.DataFrame, **kwargs) -> pd.Series:
+    """lookup function to get coordinate values from dataframe"""
+
+    # reindex frame with factorized coords
+    idx, cols = pd.factorize(coords)
+    values = frame.reindex(cols, axis=1)
+
+    # lookup values
+    values = values.to_numpy()
+    values = values[np.arange(len(values)), idx]
+
+    return pd.Series(values, index=frame.index, **kwargs)
+    
