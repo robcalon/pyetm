e607bc4b895b00ef6baf9480ff64dfa58908e542
Calon, Rob <Rob.Calon@tennet.eu>
2022-12-20 12:13:46 +0000

    fix loop

--- pyETM/utils/__init__.py
+++ pyETM/utils/__init__.py
@@ -1,5 +1,4 @@
 from .categorisation import categorise_curves, diagnose_categorisation
-from .converter import copy_study_configuration
 from .excel import add_frame, add_series
 from .interpolation import Interpolator, interpolate_clients
 from .lookup import lookup_coordinates
