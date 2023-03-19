e7c34f06f7fcc27ffe68eb6353fcc6f39d45f1eb
Calon, Rob <robcalon@outlook.com>
2023-03-19 15:30:54 +0100

    include owner
    also realligned with docs

--- src/pyetm/client/header.py
+++ src/pyetm/client/header.py
@@ -15,12 +15,12 @@ class HeaderMethods(SessionMethods):
         return self._get_scenario_header()
 
     @property
-    def area_code(self):
+    def area_code(self) -> str:
         """code for the area that the scenario describes"""
         return self._scenario_header.get('area_code')
 
     @property
-    def created_at(self):
+    def created_at(self) -> pd.Timestamp:
         """timestamp at which the scenario was created"""
 
         # get created at
@@ -33,12 +33,7 @@ class HeaderMethods(SessionMethods):
         return datetime
 
     @property
-    def display_group(self):
-        """?"""
-        return self._scenario_header.get('display_group')
-
-    @property
-    def end_year(self):
+    def end_year(self) -> int:
         """target year for which the scenario is configured"""
         return self._scenario_header.get('end_year')
 
@@ -48,48 +43,60 @@ class HeaderMethods(SessionMethods):
         return self._scenario_header.get('esdl_exportable')
 
     @property
-    def keep_compatible(self):
+    def keep_compatible(self) -> bool:
         """migrate scenario with ETM updates"""
         return self._scenario_header.get('keep_compatible')
 
     @keep_compatible.setter
-    def keep_compatible(self, boolean):
+    def keep_compatible(self, boolean: bool):
 
         # format header and update
         header = {'keep_compatible': str(bool(boolean)).lower()}
         self._update_scenario_header(header)
 
     @property
-    def metadata(self):
+    def metadata(self) -> dict:
         """metadata tags"""
         return self._scenario_header.get('metadata')
 
     @metadata.setter
-    def metadata(self, metadata):
+    def metadata(self, metadata: dict):
 
         # format header and update
         header = {'metadata': dict(metadata)}
         self._update_scenario_header(header)
 
     @property
-    def ordering(self):
-        """?"""
-        return self._scenario_header.get('ordering')
+    def owner(self) -> dict:
+        """scenario owner if created by logged in user"""
+        return self._scenario_header.get("owner")
 
     @property
-    def protected(self):
+    def private(self) -> bool:
+        """boolean that determines if the scenario is private"""
+        return self._scenario_header.get('private')
+
+    @private.setter
+    def private(self, boolean: bool):
+
+        # format header and update
+        header = {'private': str(bool(boolean)).lower()}
+        self._update_scenario_header(header)
+
+    @property
+    def protected(self) -> bool:
         """protect scenario from API changes"""
         return self._scenario_header.get('protected')
 
     @protected.setter
-    def protected(self, boolean):
+    def protected(self, boolean: bool):
 
         # format header and update
         header = {'protected': str(bool(boolean)).lower()}
         self._update_scenario_header(header)
 
     @property
-    def pro_url(self):
+    def pro_url(self) -> str:
         """get pro url for session id"""
 
         # specify base url
@@ -102,18 +109,6 @@ class HeaderMethods(SessionMethods):
         return f'{base}/scenarios/{self.scenario_id}/load'
 
     @property
-    def read_only(self):
-        """set scenario to read only"""
-        return self._scenario_header.get('read_only')
-
-    @read_only.setter
-    def read_only(self, boolean):
-
-        # format header and update
-        header = {'read_only': str(bool(boolean)).lower()}
-        self._update_scenario_header(header)
-
-    @property
     def scaling(self):
         """applied scaling factor"""
         return self._scenario_header.get('scaling')
@@ -124,17 +119,18 @@ class HeaderMethods(SessionMethods):
         return self._scenario_header.get('source')
 
     @property
-    def start_year(self):
+    def start_year(self) -> int:
         """get the reference year on which the default settings are based"""
         return self._scenario_header.get('start_year')
 
     @property
-    def template(self):
-        """get id of template scenario"""
+    def template(self) -> int | None:
+        """the id of the scenario that was used as a template,
+        or None if no template was used."""
         return str(self._scenario_header.get('template'))
 
     @property
-    def updated_at(self):
+    def updated_at(self) -> pd.Timestamp:
         """get timestamp of latest change"""
 
         # get created at
@@ -147,11 +143,11 @@ class HeaderMethods(SessionMethods):
         return datetime
 
     @property
-    def url(self):
+    def url(self) -> str:
         """get url"""
         return self._scenario_header.get('url')
 
-    def _update_scenario_header(self, header):
+    def _update_scenario_header(self, header: dict):
         """change header of scenario"""
 
         # raise without scenario id
